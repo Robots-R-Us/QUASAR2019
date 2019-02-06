d6a37b01ce62be36e4f3159d2317d900ee7cb921
matthew Back <matthewback1@gmail.com>
2019-02-06 17:04:40 -0500

    Drivetrain made
    Made dat drivetrain

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,29 @@
+
+package frc.robot;
+
+public class Constants {
+
+    // drive train
+    static int LEFT_FRONT_MOTOR = 0;
+    static  int RIGHT_FRONT_MOTOR = 1;
+    static int LEFT_REAR_MOTOR = 2;
+    static int RIGHT_REAR_MOTOR = 3;
+
+    // Elevator
+    static int ELEVATOR_MOTOR = 4;
+    static int GRABBER_PISTON = 0;
+
+    //Climber
+    static int CLIMBER_PISTON_FRONT = 1;
+    static int CLIMBER_PISTON_BACK = 2;
+
+    //CONTROLLERS
+    static int XBOX_CONTROLLER = 0;
+
+
+
+
+
+
+    
+}
